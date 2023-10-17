03a5e26f42ff84c5fbfd0937b93c81e78ed090ed
Vinicius Santos Barros <vinicius.santos230498@gmail.com>
2023-10-17 20:18:03 -0300

    Criação Tela Principal

--- src/atividade02/Atividade02.java
+++ src/atividade02/Atividade02.java
@@ -1,11 +1,19 @@
 
 package atividade02;
 
+import view.TelaPrincipal;
+
 public class Atividade02 {
 
     
     public static void main(String[] args) {
         
+        TelaPrincipal tl = new TelaPrincipal();
+        tl.setTitle("Tela Principal");
+        tl.setLocationRelativeTo(null);
+        tl.pack();
+        tl.setVisible(true);
+       
     }
     
 }
